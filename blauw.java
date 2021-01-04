77f187fcb4c9331cfcaa1a1df09fe14b348a0798
Pieter <pieterherinckx@athenuemtienen.be>
2021-01-04 16:27:32 +0100

    Initial sharing of project

---
+++ blauw.java
@@ -0,0 +1,14 @@
+
+/**
+ * class blauw - geef hier een beschrijving van deze class
+ *
+ * @author (jouw naam)
+ * @version (versie nummer of datum)
+ */
+public class blauw
+{
+    private String kleur;
+    public blauw(){
+        this.kleur = "groen";
+    }
+}
